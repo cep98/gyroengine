4b2cbe46d8e26060e054d47ddd7bd8a6a6a02e95
cep98 <christoph.hillen@gmx.net>
2025-08-04 18:43:36 +0200

    Update draw.js

--- public/js/game/draw.js
+++ public/js/game/draw.js
@@ -1,11 +1,10 @@
-import { config } from "./config.js";
-
 export function clearCanvas(ctx, canvas) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 export function drawPointer(ctx, pos) {
   ctx.beginPath();
-  ctx.arc(pos.x, pos.y, config.pointerRadius, 0, 2 * Math.PI);
+  ctx.arc(pos.x, pos.y, 10, 0, 2 * Math.PI);
+  ctx.fillStyle = "#f00";
   ctx.fill();
 }
