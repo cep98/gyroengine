0d1e54122c706f449f3fe3d0ace06dd2d3c4fcdb
cep98 <christoph.hillen@gmx.net>
2025-08-04 14:28:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,27 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
+// Statische Dateien aus dem public-Ordner bereitstellen
 app.use(express.static("public"));
 
+// WebSocket-Verbindung
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
 
-  // Weiterleiten von Gyro-Daten an alle
+  // Gyro-Daten empfangen und an alle Clients weiterleiten
   socket.on("gyroData", (data) => {
     io.emit("gyroData", { id: socket.id, ...data });
   });
 
-  // Erkennen von Client-Typen (z. B. game)
+  // Empfangen von clientType ("game", "control", etc.)
   socket.on("clientType", (data) => {
     data.id = socket.id;
     io.emit("clientType", data);
+  });
+
+  // Empfangen und Weiterleiten des Glättungswerts (0.0 – 1.0)
+  socket.on("smoothing", (value) => {
+    io.emit("smoothing", value);
   });
 
   socket.on("disconnect", () => {
@@ -24,6 +31,7 @@ io.on("connection", (socket) => {
   });
 });
 
+// Server starten
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log(`Server läuft auf http://localhost:${PORT}`);
