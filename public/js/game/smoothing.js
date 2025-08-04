e1b1c47ccdebfa481824e5675338b0fe1b94a34d
cep98 <christoph.hillen@gmx.net>
2025-08-04 18:44:18 +0200

    Update smoothing.js

--- public/js/game/smoothing.js
+++ public/js/game/smoothing.js
@@ -1,4 +1,4 @@
-export function smoothPosition(current, target, factor) {
+export function smooth(current, target, factor) {
   if (factor <= 0) return { ...target };
   return {
     x: current.x + (target.x - current.x) * factor,
