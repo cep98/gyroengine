2ed3a629ef8a7fcf0de2b2bebea766cbe6d9e72b
cep98 <christoph.hillen@gmx.net>
2025-08-04 13:58:04 +0200

    Update game.js

--- public/js/game.js
+++ public/js/game.js
@@ -2,16 +2,22 @@ let pos = { x: canvas.width / 2, y: canvas.height / 2 };
 let dir = { x: 0, y: 0 };
 
 socket.on("gyroData", (data) => {
-  dir.x = data.dirX || 0;
-  dir.y = data.dirY || 0;
+  if (data.isStart) {
+    // Kalibrierung: Punkt in Mitte setzen
+    pos = {
+      x: canvas.width / 2,
+      y: canvas.height / 2
+    };
+  } else {
+    dir.x = data.dirX || 0;
+    dir.y = data.dirY || 0;
+  }
 });
 
 function draw() {
-  // Bewegung simulieren (z. B. 5 Pixel pro Frame)
   pos.x += dir.x * 5;
   pos.y += dir.y * 5;
 
-  // Begrenzung
   pos.x = Math.max(0, Math.min(canvas.width, pos.x));
   pos.y = Math.max(0, Math.min(canvas.height, pos.y));
 
